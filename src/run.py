9a9ccfaac977bc645c96f12d0d5b6147689e0481
Aleksei Pulich <alexpulich@gmail.com>
2020-06-07 13:34:17 +0300

    implemented multiprocessing

--- src/run.py
+++ src/run.py
@@ -1,3 +1,5 @@
+from multiprocessing import Process
+
 import click
 
 from evaluation.evaluator import ThaiEvaluator
@@ -9,14 +11,22 @@ from embeddings.embeddings import load_embedding
 from cli_config import CLI_OOV_OPTION, CLI_SS_OPTION
 
 
+def _process_work(evaluator, task, embeddings, f):
+    dataset = DatasetLoader(task)
+    dataset_data = dataset.get_data()
+    result, hm = evaluator.evaluate(embeddings, dataset_data, filter_not_found=f)
+    print(task)
+    print(result, hm)
+
+
 @click.command()
 @click.option("--oov", help='Strategy to handle OOV: letters or deepcut. If empty none of them is applied')
 @click.option("--ss", help='Integrating structed sources: wn1, wn2, cn1, cn2. '
-                         'If empty only word embeddings are evaluated')
-@click.option("-f", "--filter", help='Filter not found words', is_flag=True)
+                           'If empty only word embeddings are evaluated')
+@click.option("-f", help='Filter not found words', is_flag=True)
 @click.argument("model")
 @click.argument("format")
-def run(oov, ss, model, filter, format):
+def run(oov, ss, f, model, format):
     OovStrategyCls = CLI_OOV_OPTION.get(oov, NoActionOOVStrategy)
     SSStrategyCls = CLI_SS_OPTION.get(ss, NoStructuredSourceStrategy)
 
@@ -27,13 +37,14 @@ def run(oov, ss, model, filter, format):
         structured_source_strategy=SSStrategyCls(),
     )
 
+    processes = []
     for task in tasks:
-        dataset = DatasetLoader(task)
-        dataset_data = dataset.get_data()
-        result, hm = evaluator.evaluate(embeddings, dataset_data, filter_not_found=filter)
-        print(task)
-        print(result, hm)
+        process = Process(target=_process_work, args=(evaluator, task, embeddings, f))
+        processes.append(process)
+        process.start()
 
+    for process in processes:
+        process.join()
 
 
 if __name__ == '__main__':
